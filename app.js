f115662c72898c731057c59245111e0147e80979
felprangel <feliperangel897@gmail.com>
2024-09-02 21:05:08 -0300

    adiciona configuração para views e body de requisição

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.set("views", path.join(__dirname, "/src/views"));
+app.set("view engine", "ejs");
 
 app.listen(port, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
