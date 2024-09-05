d2b2a8249bdab8f0a2e901f5aa61315277b4c0ed
felprangel <feliperangel897@gmail.com>
2024-09-04 21:29:55 -0300

    adiciona função ao controller de gennero

---
+++ src/controllers/generosController.js
@@ -0,0 +1,11 @@
+async function buscaTodosOsGeneros(req, res) {}
+
+async function criaGenero(req, res) {}
+
+async function deletaGenero(req, res) {}
+
+module.exports = {
+  buscaTodosOsGeneros,
+  criaGenero,
+  deletaGenero,
+};
