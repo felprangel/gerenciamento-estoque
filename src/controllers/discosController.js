b1be905a47a443c978615783c78f2cc8947597a6
felprangel <feliperangel897@gmail.com>
2024-09-04 21:29:44 -0300

    adiciona funções ao controller de disco

--- src/controllers/discosController.js
+++ src/controllers/discosController.js
@@ -1,1 +1,20 @@
+async function buscaTodosOsDiscos(req, res) {}
 
+async function buscaDiscoPorId(req, res) {}
+
+async function buscaDiscosPorArtista(req, res) {}
+
+async function buscaDiscosPorGenero(req, res) {}
+
+async function criaDisco(req, res) {}
+
+async function deletaDisco(req, res) {}
+
+module.exports = {
+  buscaTodosOsDiscos,
+  buscaDiscoPorId,
+  buscaDiscosPorArtista,
+  buscaDiscosPorGenero,
+  criaDisco,
+  deletaDisco,
+};
