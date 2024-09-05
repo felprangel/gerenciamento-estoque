e6ca537071097beaf6468905926e4ed06b0120f2
felprangel <feliperangel897@gmail.com>
2024-09-04 21:30:16 -0300

    adiciona funções do controller

--- src/routes/discosRouter.js
+++ src/routes/discosRouter.js
@@ -1,9 +1,12 @@
 const { Router } = require("express");
+const discosController = require("../controllers/discosController");
 const discosRouter = Router();
 
-discosRouter.get("/", (req, res) => {});
-discosRouter.get("/:id", (req, res) => {});
-discosRouter.post("/", (req, res) => {});
-discosRouter.delete("/:id", (req, res) => {});
+discosRouter.get("/", discosController.buscaTodosOsDiscos);
+discosRouter.get("/:id", discosController.buscaDiscoPorId);
+discosRouter.get("/artistas/:id", discosController.buscaDiscosPorArtista);
+discosRouter.get("/generos/:id", discosController.buscaDiscosPorGenero);
+discosRouter.post("/", discosController.criaDisco);
+discosRouter.delete("/:id", discosController.deletaDisco);
 
 module.exports = discosRouter;
