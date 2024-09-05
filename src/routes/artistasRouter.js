96680709ca8b0c128fcee03e92f7d54c5e25b2a9
felprangel <feliperangel897@gmail.com>
2024-09-04 21:23:20 -0300

    remove rota de buscar artista por id

--- src/routes/artistasRouter.js
+++ src/routes/artistasRouter.js
@@ -3,7 +3,6 @@ const artistasController = require("../controllers/artistasController");
 const artistasRouter = Router();
 
 artistasRouter.get("/", artistasController.getAllArtists);
-artistasRouter.get("/:id", artistasController.getArtistById);
 artistasRouter.post("/", artistasController.createArtist);
 artistasRouter.delete("/:id", artistasController.deleteArtist);
 
