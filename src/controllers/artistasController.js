4bbee657da57b034b8f6f279cb38a1462313af71
felprangel <feliperangel897@gmail.com>
2024-09-04 21:21:34 -0300

    adiciona funções no controller

---
+++ src/controllers/artistasController.js
@@ -0,0 +1,22 @@
+async function getAllArtists() {
+  // buscar todos os artistas
+}
+
+async function getArtistById(id) {
+  // buscar um artista pelo id
+}
+
+async function createArtist(artist) {
+  // criar um novo artista
+}
+
+async function deleteArtist(id) {
+  // deletar um artista pelo id
+}
+
+module.exports = {
+  getAllArtists,
+  getArtistById,
+  createArtist,
+  deleteArtist,
+};
